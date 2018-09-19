18b0c113a87a505e6a171af0571bd61c1dc6ab18
Derek Chan <derekchan95037@gmail.com>
2018-09-18 22:12:39 -0400

    trying again

--- src/main/java/com/gcit/lms/administrator_v2/AdministratorV2Application.java
+++ src/main/java/com/gcit/lms/administrator_v2/AdministratorV2Application.java
@@ -2,7 +2,7 @@ package com.gcit.lms.administrator_v2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 //@EnableDiscoveryClient
 @SpringBootApplication
